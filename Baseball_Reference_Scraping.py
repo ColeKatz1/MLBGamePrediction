01d195a23372f9500bec0a5bb0cfb0a051fe83b9
ColeKatz1 <84477747+ColeKatz1@users.noreply.github.com>
2022-02-18 17:41:14 -0800

    Update Baseball_Reference_Scraping.py

--- Baseball_Reference_Scraping.py
+++ Baseball_Reference_Scraping.py
@@ -1,42 +1,7 @@
-from pickle import TRUE
 import pandas, numpy
 import requests, bs4
 import re, os
-import numpy
 
-urlBaseball = "https://www.baseball-reference.com/boxes/CLE/CLE202108120.shtml"
-
-urlBaseball1 = "https://www.baseball-reference.com/boxes/CHA/CHA202108170.shtml"
-
-urlBox = "https://www.baseball-reference.com/teams/OAK/2021-schedule-scores.shtml"
-
-
-
-
-def urlOfBoxScore(url):
-
-    return url
-    
-
-def findTables(url):
-    res = requests.get(url)
-    ## The next two lines get around the issue with comments breaking the parsing.
-    comm = re.compile("<!--|-->")
-    soup = bs4.BeautifulSoup(comm.sub("", res.text), 'lxml')
-    divs = soup.findAll('div', id = "content")
-    divs = divs[0].findAll("div", id=re.compile("^all"))
-    ids = []
-    for div in divs:
-        searchme = str(div.findAll("table"))
-        x = searchme[searchme.find("id=") + 3: searchme.find(">")]
-        x = x.replace("\"", "")
-        if len(x) > 0:
-            ids.append(x)
-    return(ids)
-
-## Pulls a single table from a url provided by the user.
-## The desired table should be specified by tableID.
-## This function is used in all functions that do more complicated pulls.
 def pullTable(url, tableID):
     res = requests.get(url)
     ## Work around comments
@@ -58,22 +23,6 @@ def pullTable(url, tableID):
     #data = data.loc[data[header[0]] != header[0]]
     data = data.drop(0)
     data = data.reset_index(drop = True)
-    return(data)
-
-def pullHeader(url, tableID):
-    res = requests.get(url)
-    ## Work around comments
-    comm = re.compile("<!--|-->")
-    soup = bs4.BeautifulSoup(comm.sub("", res.text), 'lxml')
-    tables = soup.findAll('table', id = tableID)
-    data_rows = tables[0].findAll('tr')
-    data_header = tables[0].findAll('thead')
-    data_header = data_header[0].findAll("tr")
-    data_header = data_header[0].findAll("th")
-    game_data = [[td.getText() for td in data_rows[i].findAll(['th','td'])]
-        for i in range(1)
-        ]
-    data = pandas.DataFrame(game_data)
     return(data)
 
 def pullBattingData(url):
