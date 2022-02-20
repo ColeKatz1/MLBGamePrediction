3e544d9d1a0a41a531c6efd971e0218d1259ee97
ColeKatz1 <84477747+ColeKatz1@users.noreply.github.com>
2022-02-20 10:53:12 -0800

    Update Baseball_Reference_Scraping.py
    Added svc linear machine learning method. The results vary between 53% and 60% accuracy using only batter data for one team. This dataset does not yet factor in pitching or the opponent

--- Baseball_Reference_Scraping.py
+++ Baseball_Reference_Scraping.py
@@ -1,9 +1,16 @@
 from cmath import nan
 from contextlib import nullcontext
+from random import seed
 from turtle import home
 import pandas, numpy
 import requests, bs4
 import re, os
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import glob
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier 
+import random
 
 def findTables(url):
     res = requests.get(url)
@@ -144,7 +151,10 @@ def getSeasonStats(url, team, teamFullName):
     for i in urlList:
         battingData = pullBattingData(i, teamFullName)
         battingDataList.append(battingData)
-    battingDataList = numpy.reshape(battingDataList, (162,24))
+    if len(battingDataList) == 162:
+        battingDataList = numpy.reshape(battingDataList, (162,24))
+    elif len(battingDataList) == 161:
+        battingDataList = numpy.reshape(battingDataList, (161,24))
     dfOfBattingData = pandas.DataFrame(battingDataList) 
     dfOfBattingData.columns = ['Batting','AB','R','H','RBI','BB','SO','PA','BA','OBP','SLG','OPS','Pit','Str','WPA','aLI','WPA+','WPA-','cWPA','acLI','RE24','PO','A','Details']
     dfOfBattingData = dfOfBattingData.drop(['Batting','WPA','aLI','WPA+','WPA-','cWPA','acLI','PO','A','Details'], axis=1)
@@ -184,21 +194,25 @@ def addSeasonLongAverageStatistics(df, stat):
     variableList = df[stat]
     totalSum = 0
     totalCount = 0
+    variableContinuedStatList.insert(0,nan)
 
     for i in range(len(variableList)):
         totalSum = totalSum + variableList[i]
         totalCount = totalCount + 1
         average = totalSum/totalCount
         variableContinuedStatList.append(average)
+    variableContinuedStatList.pop()
     df[stat + '_Season_Long_Average'] = variableContinuedStatList
 
 def addSeasonLongCount(df, stat):
     countTotalList = []
     variableList = df[stat]
     count = 0
+    countTotalList.insert(0,nan)
     for i in range(len(variableList)):
         count = count + int(variableList[i])
         countTotalList.append(count)
+    countTotalList.pop()
     df[stat + "_Season_Long_Count"] = countTotalList
 
 
@@ -208,6 +222,7 @@ def transformedSeasonStats(df):
     winsTotal = 0
     gamesTotal = 0
     winningPercentageList = []
+    winningPercentageList.insert(0,nan)
     for i in range(len(winsList)):
         if winsList[i] == 0:
             winsTotal = winsTotal + 1
@@ -218,6 +233,7 @@ def transformedSeasonStats(df):
             gamesTotal = gamesTotal + 1
             winningPercentage = winsTotal / gamesTotal
             winningPercentageList.append(winningPercentage)
+    winningPercentageList.pop()
     df['Win_Percentage'] = winningPercentageList
     addSeasonLongCount(df,'R')
     addSeasonLongCount(df,'H')
@@ -245,21 +261,90 @@ def completedBattingStatsOfTeamdf(url, team, teamFullName):
     getSeasonStats(url, team, teamFullName)
     df = pandas.read_csv(teamFullName + "_Batting_Statistics.csv")
     transformedDf = transformedSeasonStats(df)
-    return(transformedDf)
+    transformedDf.to_csv(teamFullName + "_Completed_Batting_Statistics.csv")
+    #return(transformedDf)
 
 
 def completedBattingStatsOfAllTeams():
-    allScheduleUrlList = ["https://www.baseball-reference.com/teams/ARI/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/ATL/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/BAL/2021-schedule-scores.shtml", "https://www.baseball-reference.com/teams/BOS/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/CHW/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/CHC/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/CIN/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/CLE/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/COL/2021.shtml","https://www.baseball-reference.com/teams/DET/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/HOU/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/KCR/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/LAA/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/LAD/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/MIA/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/MIL/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/MIN/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/NYY/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/NYM/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/OAK/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/PHI/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/PIT/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/SDP/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/SFG/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/SEA/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/STL/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/TBR/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/TEX/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/TOR/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/WSN/2021-schedule-scores.shtml"]
-    allTeamList = ['ARI','ATL','BAL','BOS','CHW','CHC','CIN','CLE','COL','DET','HOU','KCR','LAA','LAD','MIA','MIL','MIN','NYY','NYM','OAK','PHI','PIT','SDP','SFG','SEA','STL','TBR','TEX','TOR','WAS']
+    allScheduleUrlList = ["https://www.baseball-reference.com/teams/ARI/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/ATL/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/BAL/2021-schedule-scores.shtml", "https://www.baseball-reference.com/teams/BOS/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/CHW/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/CHC/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/CIN/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/CLE/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/COL/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/DET/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/HOU/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/KCR/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/LAA/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/LAD/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/MIA/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/MIL/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/MIN/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/NYY/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/NYM/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/OAK/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/PHI/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/PIT/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/SDP/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/SFG/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/SEA/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/STL/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/TBR/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/TEX/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/TOR/2021-schedule-scores.shtml","https://www.baseball-reference.com/teams/WSN/2021-schedule-scores.shtml"]
+    allTeamList = ['ARI','ATL','BAL','BOS','CHW','CHC','CIN','CLE','COL','DET','HOU','KCR','LAA','LAD','MIA','MIL','MIN','NYY','NYM','OAK','PHI','PIT','SDP','SFG','SEA','STL','TBR','TEX','TOR','WSN']
     allTeamFullNameList = ["ArizonaDiamondbacks","AtlantaBraves","BaltimoreOrioles","BostonRedSox","ChicagoWhiteSox","ChicagoCubs","CincinnatiReds","ClevelandIndians","ColoradoRockies","DetroitTigers","HoustonAstros","KansasCityRoyals","LosAngelesAngels","LosAngelesDodgers","MiamiMarlins","MilwaukeeBrewers","MinnesotaTwins","NewYorkYankees","NewYorkMets","OaklandAthletics","PhiladelphiaPhillies","PittsburghPirates","SanDiegoPadres","SanFranciscoGiants","SeattleMariners","StLouisCardinals","TampaBayRays","TexasRangers","TorontoBlueJays","WashingtonNationals"]
-    # only need 1 for loop to go through each team
-    for i in range(len(allTeamList)):
+    for i in range(len(allScheduleUrlList)):
+        print(allTeamFullNameList[i])
         completedBattingStatsOfTeamdf(allScheduleUrlList[i],allTeamList[i],allTeamFullNameList[i])
-        
 
-print(getSeasonStats("https://www.baseball-reference.com/teams/STL/2021-schedule-scores.shtml",'STL',"StLouisCardinals"))
+def combineToOneDataFrame():
+    ARIdf = pandas.read_csv('ArizonaDiamondbacks_Completed_Batting_Statistics.csv')
+    ATLdf = pandas.read_csv('AtlantaBraves_Completed_Batting_Statistics.csv')
+    BALdf = pandas.read_csv('BaltimoreOrioles_Completed_Batting_Statistics.csv')
+    BOSdf = pandas.read_csv('BostonRedSox_Completed_Batting_Statistics.csv')
+    CHWdf = pandas.read_csv('ChicagoWhiteSox_Completed_Batting_Statistics.csv')
+    CHCdf = pandas.read_csv('ChicagoCubs_Completed_Batting_Statistics.csv')
+    CINdf = pandas.read_csv('CincinnatiReds_Completed_Batting_Statistics.csv')
+    CLEdf = pandas.read_csv('ClevelandIndians_Completed_Batting_Statistics.csv')
+    COLdf = pandas.read_csv('ColoradoRockies_Completed_Batting_Statistics.csv')
+    DETdf = pandas.read_csv('DetroitTigers_Completed_Batting_Statistics.csv')
+    HOUdf = pandas.read_csv('HoustonAstros_Completed_Batting_Statistics.csv')
+    KCRdf = pandas.read_csv('KansasCityRoyals_Completed_Batting_Statistics.csv')
+    LAAdf = pandas.read_csv('LosAngelesAngels_Completed_Batting_Statistics.csv')
+    LADdf = pandas.read_csv('LosAngelesDodgers_Completed_Batting_Statistics.csv')
+    MIAdf = pandas.read_csv('MiamiMarlins_Completed_Batting_Statistics.csv')
+    MILdf = pandas.read_csv('MilwaukeeBrewers_Completed_Batting_Statistics.csv')
+    MINdf = pandas.read_csv('MinnesotaTwins_Completed_Batting_Statistics.csv')
+    NYYdf = pandas.read_csv('NewYorkYankees_Completed_Batting_Statistics.csv')
+    NYMdf = pandas.read_csv('NewYorkMets_Completed_Batting_Statistics.csv')
+    OAKdf = pandas.read_csv('OaklandAthletics_Completed_Batting_Statistics.csv')
+    PHIdf = pandas.read_csv('PhiladelphiaPhillies_Completed_Batting_Statistics.csv')
+    PITdf = pandas.read_csv('PittsburghPirates_Completed_Batting_Statistics.csv')
+    SDPdf = pandas.read_csv('SanDiegoPadres_Completed_Batting_Statistics.csv')
+    SFGdf = pandas.read_csv('SanfranciscoGiants_Completed_Batting_Statistics.csv')
+    SEAdf = pandas.read_csv('SeattleMariners_Completed_Batting_Statistics.csv')
+    STLdf = pandas.read_csv('StLouisCardinals_Completed_Batting_Statistics.csv')
+    TBRdf = pandas.read_csv('TampaBayRays_Completed_Batting_Statistics.csv')
+    TEXdf = pandas.read_csv('TexasRangers_Completed_Batting_Statistics.csv')
+    TORdf = pandas.read_csv('TorontoBlueJays_Completed_Batting_Statistics.csv')
+    WASdf = pandas.read_csv('WashingtonNationals_Completed_Batting_Statistics.csv')
+    dfComplete = pandas.concat([ARIdf,ATLdf,BALdf,BOSdf,CHWdf,CHCdf,CINdf,CLEdf,COLdf,DETdf,HOUdf,KCRdf,LAAdf,LADdf,MIAdf,MILdf,MINdf,NYYdf,NYMdf,OAKdf,PHIdf,PITdf,SDPdf,SFGdf,SEAdf,STLdf,TBRdf,TEXdf,TORdf,WASdf])
+    dfComplete.to_csv('dfComplete.csv')
+    return(dfComplete)
 
+def createRatioVariables(df):
+    # maybe check if opponent and url is the same, then you can make a new dataframe from this one with the required math
+    # so if url matches, then add a row to the new data frame with the team divided by the opponents stats to form the ratio and then you can also add the win loss column after
+    # all we need to do is find the matching urls and then divide the first row by the row with the matching url
+    winOrLoss = df.filter(['WinOrLoss','url'])
+    winOrLossDf = pandas.DataFrame(winOrLoss)
+    df = df.drop(['AB','R','H','RBI','BB','SO','PA','BA','OBP','SLG','OPS','Pit','Str','RE24','WinOrLoss','HomeOrAway','Team','Opponent'], axis = 1)
+    df = df.dropna(axis=0)
+    df = df.iloc[:,3:]
+    df = df.values.tolist()
+    return(df)
 
+#print(combineToOneDataFrame())
+#print(completedBattingStatsOfAllTeams())
 
+def pullPitching(url, team):
+    teamBatting = pullTable(url, team + "batting")
+    teamBatting = teamBatting.loc[[len(teamBatting)-1]]
+    return(teamBatting)
 
+#print(findTables("https://www.baseball-reference.com/boxes/OAK/OAK202104030.shtml"))
+
+df = pandas.read_csv('dfComplete.csv')
+#dfFull = dfFull.drop(['AB','R','H','RBI','BB','SO','PA','BA','OBP','SLG','OPS','Pit','Str','RE24','WinOrLoss'], axis = 1)
+
+df = df.dropna(axis = 0)
+y = df['WinOrLoss']
+
+x = df.drop(['AB','R','H','RBI','BB','SO','PA','BA','OBP','SLG','OPS','Pit','Str','RE24','WinOrLoss','Team','Opponent','url'],axis=1)
+
+for i in range(100):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+    svc_lin = SVC(kernel = 'linear', )
+    svc_lin_model = svc_lin.fit(x_train, y_train)
+    print('SVC Linear:', svc_lin_model.score(x_test, y_test))
+    #score = svc_lin_model.score(x_test, y_test)
+    #totalScore = totalScore + score
+    #count = count + 1
+    #print(totalScore/count)
 
