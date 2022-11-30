744f0f9bce31f51909291f8421e2f8456699c119
ColeKatz1 <84477747+ColeKatz1@users.noreply.github.com>
2022-11-30 18:44:53 -0500

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -1,6 +1,6 @@
-import Baseball_Reference_Scraping_Functions
+import Baseball_Reference_Scraping_Functions as br
 
-Baseball_Reference_Scraping_Functions.completedBattingStatsOfAllTeams() #creates a dataset of completed batting stats for every team
-Baseball_Reference_Scraping_Functions.combineToOneDataFrame() #combines all these datasets into one
-Baseball_Reference_Scraping_Functions.addFinalFeatures() #prepares data for machine learning purposes by combining data between teams for each game they play against each other
+br.completedBattingStatsOfAllTeams() #creates a dataset of completed batting stats for every team
+br.combineToOneDataFrame() #combines all these datasets into one
+br.addFinalFeatures() #prepares data for machine learning purposes by combining data between teams for each game they play against each other
 
